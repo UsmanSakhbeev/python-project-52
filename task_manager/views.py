4ae1f1f96dcfeb225169d402aec8ee154d377153
UsmanSakhbeev <sahbeyev@gmail.com>
2025-02-21 10:46:11 +0300

    updated login

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,13 @@
+from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.contrib.auth.views import LogoutView as DjangoLogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
 
-class LoginView(DjangoLoginView):
+class LoginView(SuccessMessageMixin, DjangoLoginView):
     template_name = "form.html"
     form_class = AuthenticationForm
     next_page = reverse_lazy("index")
@@ -15,3 +17,7 @@ class LoginView(DjangoLoginView):
 
 class LogoutView(DjangoLogoutView):
     next_page = reverse_lazy("index")
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(request, messages.INFO, _("You were logged out"))
+        return super().dispatch(request, *args, **kwargs)
